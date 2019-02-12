e690e0999e971182af6989154d6bd9041eb9f57f
peterforme <1009480326@qq.com>
2019-02-12 16:40:31 +0800

    之前忘记加service注解了，category模块初步的插删改查

--- ssmlearn/my/tmall_ssm/src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
+++ ssmlearn/my/tmall_ssm/src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
@@ -1,8 +1,20 @@
 package com.how2java.tmall.mapper;
 
 import com.how2java.tmall.pojo.Category;
+import com.how2java.tmall.util.Page;
+
 import java.util.List;
 
 public interface CategoryMapper {
-	List<Category> list();
+	public List<Category> list(Page page);
+	 
+    public int total();
+    
+    void add(Category category);
+    
+    void delete(int id);
+    
+    Category get(int id);
+    
+    void update(Category category);
 }
