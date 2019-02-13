156321809097af456915ba09df600cdc459b325c
peterforme <1009480326@qq.com>
2019-02-13 15:52:20 +0800

    使用逆向工程生成

--- ssmlearn/my/tmall_ssm/src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
+++ ssmlearn/my/tmall_ssm/src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
@@ -1,17 +1,21 @@
 package com.how2java.tmall.mapper;
- 
+
 import com.how2java.tmall.pojo.Category;
- 
+import com.how2java.tmall.pojo.CategoryExample;
 import java.util.List;
- 
+
 public interface CategoryMapper {
-     List<Category> list();
- 
-     void add(Category category);
- 
-     void delete(int id);
- 
-     Category get(int id);
- 
-     void update(Category category);
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Category record);
+
+    int insertSelective(Category record);
+
+    List<Category> selectByExample(CategoryExample example);
+
+    Category selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Category record);
+
+    int updateByPrimaryKey(Category record);
 }
--- ssmlearn/my/tmall_ssm/src/main/java/com/how2java/tmall/service/impl/CategoryServiceImpl.java
+++ ssmlearn/my/tmall_ssm/src/main/java/com/how2java/tmall/service/impl/CategoryServiceImpl.java
@@ -2,6 +2,7 @@ package com.how2java.tmall.service.impl;
  
 import com.how2java.tmall.mapper.CategoryMapper;
 import com.how2java.tmall.pojo.Category;
+import com.how2java.tmall.pojo.CategoryExample;
 import com.how2java.tmall.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,26 +16,28 @@ public class CategoryServiceImpl implements CategoryService {
  
     @Override
     public List<Category> list() {
-        return categoryMapper.list();
+        CategoryExample example =new CategoryExample();
+        example.setOrderByClause("id desc");
+        return categoryMapper.selectByExample(example);
     }
  
     @Override
     public void add(Category category) {
-        categoryMapper.add(category);
+        categoryMapper.insert(category);
     }
  
     @Override
     public void delete(int id) {
-        categoryMapper.delete(id);
+        categoryMapper.deleteByPrimaryKey(id);
     }
  
     @Override
     public Category get(int id) {
-        return categoryMapper.get(id);
+        return categoryMapper.selectByPrimaryKey(id);
     }
  
     @Override
     public void update(Category category) {
-        categoryMapper.update(category);
+        categoryMapper.updateByPrimaryKeySelective(category);
     }
 }
