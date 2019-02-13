65b653b023433a7faa828cf9bfc5d19da2b7005e
peterforme <1009480326@qq.com>
2019-02-13 14:37:51 +0800

    分页重构完成

--- ssmlearn/my/tmall_ssm/src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
+++ ssmlearn/my/tmall_ssm/src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
@@ -1,20 +1,17 @@
 package com.how2java.tmall.mapper;
-
+ 
 import com.how2java.tmall.pojo.Category;
-import com.how2java.tmall.util.Page;
-
+ 
 import java.util.List;
-
+ 
 public interface CategoryMapper {
-	public List<Category> list(Page page);
-	 
-    public int total();
-    
-    void add(Category category);
-    
-    void delete(int id);
-    
-    Category get(int id);
-    
-    void update(Category category);
+     List<Category> list();
+ 
+     void add(Category category);
+ 
+     void delete(int id);
+ 
+     Category get(int id);
+ 
+     void update(Category category);
 }
--- ssmlearn/my/tmall_ssm/src/main/java/com/how2java/tmall/service/CategoryService.java
+++ ssmlearn/my/tmall_ssm/src/main/java/com/how2java/tmall/service/CategoryService.java
@@ -1,14 +1,16 @@
 package com.how2java.tmall.service;
  
 import com.how2java.tmall.pojo.Category;
-import com.how2java.tmall.util.Page;
 import java.util.List;
  
 public interface CategoryService{
-    int total();
-    List<Category> list(Page page);
+    List<Category> list();
+ 
     void add(Category category);
+ 
     void delete(int id);
+ 
     Category get(int id);
+ 
     void update(Category category);
 }
