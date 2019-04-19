ec64917c97cb6016c41e69130069f8b1f86a438b
peterforme <1009480326@qq.com>
2019-04-19 16:23:48 +0800

    产品属性值管理完成

--- springbootlearn/my/src/main/java/com/how2java/tmall/dao/PropertyDAO.java
+++ springbootlearn/my/src/main/java/com/how2java/tmall/dao/PropertyDAO.java
@@ -11,4 +11,6 @@ import com.how2java.tmall.pojo.Property;
  
 public interface PropertyDAO extends JpaRepository<Property,Integer>{
     Page<Property> findByCategory(Category category, Pageable pageable);
+    List<Property> findByCategory(Category category);
+ 
 }
