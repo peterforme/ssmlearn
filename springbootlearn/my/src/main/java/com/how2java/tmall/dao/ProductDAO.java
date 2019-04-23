a993b4e06b0d8bde932d1c6ce19a8d0d34fecb9a
peterforme <1009480326@qq.com>
2019-04-23 17:00:54 +0800

    前台的首页完成

--- springbootlearn/my/src/main/java/com/how2java/tmall/dao/ProductDAO.java
+++ springbootlearn/my/src/main/java/com/how2java/tmall/dao/ProductDAO.java
@@ -1,14 +1,14 @@
 package com.how2java.tmall.dao;
-  
-import java.util.List;
  
+import com.how2java.tmall.pojo.Category;
+import com.how2java.tmall.pojo.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
  
-import com.how2java.tmall.pojo.Category;
-import com.how2java.tmall.pojo.Product;
+import java.util.List;
  
 public interface ProductDAO extends JpaRepository<Product,Integer>{
     Page<Product> findByCategory(Category category, Pageable pageable);
+    List<Product> findByCategoryOrderById(Category category);
 }
