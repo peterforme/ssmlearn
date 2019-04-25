c45b9a5fb20b745ea21b53e4bd84ab8c9e0831c2
peterforme <1009480326@qq.com>
2019-04-25 17:04:13 +0800

    加入了搜索技术es

--- springbootlearn/my/src/main/java/com/how2java/tmall/Application.java
+++ springbootlearn/my/src/main/java/com/how2java/tmall/Application.java
@@ -11,9 +11,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import com.how2java.tmall.util.PortUtil;
 @SpringBootApplication
 @EnableCaching
+@EnableElasticsearchRepositories(basePackages = "com.how2java.tmall.es")
+@EnableJpaRepositories(basePackages = {"com.how2java.tmall.dao", "com.how2java.tmall.pojo"})
 public class Application {
     static {
         PortUtil.checkPort(6379,"Redis 服务端",true);
+        PortUtil.checkPort(9300,"ElasticSearch 服务端",true);
+        PortUtil.checkPort(5601,"Kibana 工具", true);
     }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- springbootlearn/my/src/main/java/com/how2java/tmall/service/ProductService.java
+++ springbootlearn/my/src/main/java/com/how2java/tmall/service/ProductService.java
@@ -1,10 +1,14 @@
 package com.how2java.tmall.service;
  
 import com.how2java.tmall.dao.ProductDAO;
+import com.how2java.tmall.es.ProductESDAO;
 import com.how2java.tmall.pojo.Category;
 import com.how2java.tmall.pojo.Product;
 import com.how2java.tmall.util.Page4Navigator;
 import com.how2java.tmall.util.SpringContextUtil;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.functionscore.FunctionScoreQueryBuilder;
+import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
@@ -13,6 +17,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +28,8 @@ import java.util.List;
 public class ProductService  {
      
     @Autowired ProductDAO productDAO;
+    @Autowired
+    ProductESDAO productESDAO;
     @Autowired ProductImageService productImageService;
     @Autowired CategoryService categoryService;
     @Autowired OrderItemService orderItemService;
@@ -30,11 +38,13 @@ public class ProductService  {
     @CacheEvict(allEntries=true)
     public void add(Product bean) {
         productDAO.save(bean);
+        productESDAO.save(bean);
     }
  
     @CacheEvict(allEntries=true)
     public void delete(int id) {
         productDAO.delete(id);
+        productESDAO.delete(id);
     }
  
     @Cacheable(key="'products-one-'+ #p0")
@@ -45,6 +55,7 @@ public class ProductService  {
     @CacheEvict(allEntries=true)
     public void update(Product bean) {
         productDAO.save(bean);
+        productESDAO.save(bean);
     }
  
     @Cacheable(key="'products-cid-'+#p0+'-page-'+#p1 + '-' + #p2 ")
@@ -104,10 +115,31 @@ public class ProductService  {
     }
  
     public List<Product> search(String keyword, int start, int size) {
-        Sort sort = new Sort(Sort.Direction.DESC, "id");
-        Pageable pageable = new PageRequest(start, size, sort);
-        List<Product> products =productDAO.findByNameLike("%"+keyword+"%",pageable);
-        return products;
+        initDatabase2ES();
+        FunctionScoreQueryBuilder functionScoreQueryBuilder = QueryBuilders.functionScoreQuery()
+                .add(QueryBuilders.matchPhraseQuery("name", keyword),
+                        ScoreFunctionBuilders.weightFactorFunction(100))
+                .scoreMode("sum")
+                .setMinScore(10);
+        Sort sort  = new Sort(Sort.Direction.DESC,"id");
+        Pageable pageable = new PageRequest(start, size,sort);
+        SearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withPageable(pageable)
+                .withQuery(functionScoreQueryBuilder).build();
+ 
+        Page<Product> page = productESDAO.search(searchQuery);
+        return page.getContent();
+    }
+ 
+    private void initDatabase2ES() {
+        Pageable pageable = new PageRequest(0, 5);
+        Page<Product> page =productESDAO.findAll(pageable);
+        if(page.getContent().isEmpty()) {
+            List<Product> products= productDAO.findAll();
+            for (Product product : products) {
+                productESDAO.save(product);
+            }
+        }
     }
  
 }
