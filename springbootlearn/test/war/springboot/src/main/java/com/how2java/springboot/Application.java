744a0dfd183cf955ae918a24003a84e7e650ba2c
peterforme <1009480326@qq.com>
2019-04-28 15:51:14 +0800

    添加resources节点才可以用jar方式打包

---
+++ springbootlearn/test/war/springboot/src/main/java/com/how2java/springboot/Application.java
@@ -0,0 +1,21 @@
+package com.how2java.springboot;
+ 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+ 
+@SpringBootApplication
+@ServletComponentScan
+public class Application extends SpringBootServletInitializer {
+ 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
