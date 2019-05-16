7070be0590ba1770545fa7d50bc2bb244f6bf279
peterforme <1009480326@qq.com>
2019-05-16 14:10:29 +0800

    聚合监控完成

--- springcloudlearn/springclound/product-view-service-feign/src/main/java/cn/how2j/springcloud/util/AccessViewService.java
+++ springcloudlearn/springclound/product-view-service-feign/src/main/java/cn/how2j/springcloud/util/AccessViewService.java
@@ -9,15 +9,19 @@ public class AccessViewService {
          
         while(true) {
             ThreadUtil.sleep(1000);
-            try {
-                String html= HttpUtil.get("http://127.0.0.1:8012/products");
-                System.out.println("html length:" + html.length());
-            }
-            catch(Exception e) {
-                System.err.println(e.getMessage());
-            }
- 
+            access(8012);
+            access(8013);
         }
          
+    }
+ 
+    public static void access(int port) {
+        try {
+            String html= HttpUtil.get(String.format("http://127.0.0.1:%d/products",port));
+            System.out.printf("%d 地址的视图服务访问成功，返回大小是 %d%n" ,port, html.length());
+        }
+        catch(Exception e) {
+            System.err.printf("%d 地址的视图服务无法访问%n",port);
+        }
     }
 }
